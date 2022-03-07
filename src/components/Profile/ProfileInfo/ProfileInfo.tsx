a43e42966a7ce15f9031677b1a566987d9798303
Александр <80578847+PiramEVR@users.noreply.github.com>
2022-03-07 19:42:06 +0300

    Status

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -3,9 +3,12 @@ import s from './ProfileInfo.module.css';
 import Preloader from "../../../common/Preloader/Preloader";
 import {ProfileType} from "../../../redux/profileReducer";
 import usersIMG from '../../../assets/images/user.jpg'
+import ProfileStatus from "./ProfileStatus";
 
 type ProfileInfoPropsType = {
     profile: ProfileType | null
+    status: string
+    updateUserStatus:(status: string)=>void
 }
 
 function ProfileInfo(props: ProfileInfoPropsType) {
@@ -15,7 +18,7 @@ function ProfileInfo(props: ProfileInfoPropsType) {
             <img className={s.content} src={props.profile.photos.large
                 ? props.profile.photos.large
                 : usersIMG}/>
-            description
+            <ProfileStatus status={props.status} updateUserStatus={props.updateUserStatus}/>
         </div>
     )
 }
---
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -0,0 +1,58 @@
+import React, {ChangeEvent} from "react";
+
+type ProfileStatusPropsType = {
+    status: string
+    updateUserStatus: (status: string) => void
+}
+
+class ProfileStatus extends React.Component <ProfileStatusPropsType> {
+    state = {
+        editMode: false,
+        status: this.props.status,
+    }
+    activateEditMode = () => {
+        this.setState({
+            editMode: true
+        })
+    }
+    deactivateEditMode = () => {
+        this.setState({
+            editMode: false
+        });
+        this.props.updateUserStatus(this.state.status)
+    }
+    onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
+        this.setState({
+            status: e.currentTarget.value
+        })
+        console.log(e.currentTarget.value)
+    }
+
+    componentDidUpdate(prevProps: Readonly<ProfileStatusPropsType>, prevState: Readonly<{}>, snapshot?: any) {
+        if(prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {!this.state.editMode &&
+                    <div>
+                        <span onDoubleClick={this.activateEditMode}>{this.props.status || '-----'}</span>
+                    </div>
+                }
+                {this.state.editMode &&
+                    <div>
+                        <input onBlur={this.deactivateEditMode} value={this.state.status}
+                               onChange={this.onStatusChange} autoFocus/>
+                    </div>
+                }
+            </div>
+        )
+    }
+}
+
+export default ProfileStatus;
